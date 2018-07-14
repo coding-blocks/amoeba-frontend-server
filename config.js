b6209ab7dbd19e2bd4e21c29a16bc7bd61e3a7f2
fire-finch <firefinchdev@gmail.com>
2018-07-14 14:03:58 +0530

    Add: Implement DOM for <noscript> for index

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
+let port = 8080;
+let base_link = 'https://api-online.cb.lk';
 module.exports = {
-  PORT: 8080
+  PORT: port,
+  API: {
+    BASE_URL:`${base_link}/api/v2/`,
+    COURSE_URL: `${base_link}/api/v2/courses/`,
+    RECOMMENDED_COURSE: `${base_link}/api/v2/courses?filter%5Brecommended%5D=true&filter%5Bunlisted%5D=false&include=instructors%2Cruns&sort=difficulty`,
+    ANNOUNCEMENTS: `${base_link}/api/v2/carousel_cards/`
+  }
 }
--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan')
 const cheerio = require('cheerio')
 const fs = require('fs-extra')
 const path = require('path')
-const { cookHTML } = require('./utils')
+const deserializer = require('jsonapi-deserializer').deserialize;
+const { cookHTML, getAPIdata } = require('./utils')
+
+//const courseRouter = require('./routes/courses')
 
 const app = express()
 const config = require('./config.js')
@@ -12,12 +15,49 @@ const port = config.PORT || '8080'
 
 app.use(morgan('dev'))
 
-app.get('/', async (req, res) => {
-  const html = await cookHTML('index', {name: 'abhishek'})
+app.get('/', async (req, res, next) => {
+
+  let dataFetch = {}
+  //TODO: should the url be static?
+  await getAPIdata(config.API.RECOMMENDED_COURSE)
+    .then((courses) => {
+      dataFetch.courses = deserializer(courses)
+    }).catch((e) => {
+      console.log(e);
+      next();
+    })
+
+  await getAPIdata(config.API.ANNOUNCEMENTS)
+    .then((list) => {
+      dataFetch.announcements = list.data;
+    }).catch((e) => {
+      console.log(e);
+      next();
+    })
+
+  //res.json(dataFetch.courses)
+
+  const html = await cookHTML('index', {
+    baseUrlApi: config.API.BASE_URL,
+    courses: dataFetch.courses,
+    announcements: dataFetch.announcements,
+    epoch: Math.round((new Date()).getTime() / 1000)
+  })
+
+  // res.json(dataFetch.courses)
   res.send(html)
 })
 
+
+
 app.use(express.static('dist'))
+
+
+app.get('*', async (req, res) => {
+  const html = await cookHTML('forward', {})
+  res.send(html)
+})
+
 
 app.listen(config.PORT || '8080', function () {
   console.log('Serving Content on', port)
