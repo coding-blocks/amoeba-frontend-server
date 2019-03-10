61ac71ba9918ad5477f02d3d2da9a799ac465691
Abhishek Gupta <zapper.awake@gmail.com>
2019-03-10 16:23:16 +0530

    update recommended courses endpoint

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     BASE_URL:`${base_link_api}/api/v2/`,
     COURSE_URL: `${base_link_api}/api/v2/courses/`,
     COURSE_URL_WITH_INST_RUNS: `${base_link_api}/api/v2/courses?include=instructors%2Cruns`,
-    RECOMMENDED_COURSE: `${base_link_api}/api/v2/courses?filter%5Brecommended%5D=true&filter%5Bunlisted%5D=false&include=instructors%2Cruns&sort=difficulty`,
+    RECOMMENDED_COURSE: `${base_link_api}/api/v2/courses?exclude=ratings&filter%5Brecommended%5D=true&filter%5Bunlisted%5D=false&include=instructors%2Cruns&sort=difficulty`,
     ANNOUNCEMENTS: `${base_link_api}/api/v2/carousel_cards/`,
     COURSE_LIST: `${base_link_api}/api/v2/courses/list`
   }
